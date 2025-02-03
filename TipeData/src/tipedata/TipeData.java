3fb7590f623a8dbdee650a981200f62339ea0742
Alfaraby Indra <alfarabyindra@gmail.com>
2025-02-03 11:13:26 +0700

    commit coding java

---
+++ TipeData/src/tipedata/TipeData.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author User
+ */
+public class TipeData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // tipe data java
+
+        
+/* 
+ A. Primitive type data       
+ 1. byte
+ 2. short
+ 3. int
+ 4. long       
+ 5. float
+ 6. double
+ 7. boolean
+ 8. char
+ 
+ B. Non-Primitive type data
+ 1. String
+ 2. Array   
+ 3. Class
+ 4. Interfaces, etc.       
+*/        
+
+//membuat variabel
+String nama, alamat;
+int usia;
+double tinggi;
+
+
+//mengisi nilai variabel
+nama= "Alfaraby Indra";
+alamat = "Pekanbaru";
+usia = 26;
+tinggi = 170.0;
+
+
+//mencetak variabel yang diisi
+System.out.println("Nama:" + nama);
+System.out.println("Usia:"+ usia + " Tahun");
+System.out.println("Alamat:"+alamat);
+System.out.println("Tinggi:"+tinggi+" cm");
+    }
+    
+}
